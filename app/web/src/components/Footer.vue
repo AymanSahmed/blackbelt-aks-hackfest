896c8391b6dcfbce98de0ac5b584657d7c82ef93
AymanSahmed <40923112+AymanSahmed@users.noreply.github.com>
2019-02-07 15:27:28 -0800

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team - AymanS01
+        Azure Global Blackbelt Team - AymanS0111
       </div>
       <div class="col-lg-6">
       </div>
